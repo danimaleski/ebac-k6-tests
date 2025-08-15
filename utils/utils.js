b91829570a43b1b97c71f7b20851ef29f5aabf96
Daniella Maleski <daniella.maleski@gmail.com>
2025-08-15 18:01:07 -0300

    utils alteraçoes

--- utils/utils.js
+++ utils/utils.js
@@ -1,10 +1,5 @@
 export default class Utils {
     static getBaseUrl(){
-        switch(process.env.NODE_ENV){
-            case 'development':
-                return `http://host.docker.internal:3000/api`
-            case 'production':
-                return `http://localhost:3000/api`
-        }
+        return `https://localhost:3000/api`
     }
 }
